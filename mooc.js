148f89145f7a05ac511a9866f074459599826d0b
kurmyworld <741297479@qq.com>
2016-03-24 15:29:16 +0800

    变量定义放外边，初始化放函数里边

--- mooc.js
+++ mooc.js
@@ -1,10 +1,5 @@
-var patt = new RegExp("[0-9][0-9]:[0-9][0-9]");
-var datapart = document.getElementsByClassName("bottom-part");
-var data;
 var i=0;
-var dd;
-var dl;
-var index;
+var datapart;
 function next(){
 	alert("课程已挂完，请点击下一课！");
 	if(++i<datapart.length){
@@ -12,6 +7,9 @@ function next(){
 		goon();
 		return;
 	}
+	var dd;
+	var dl;
+	var index;
 	dd = document.getElementsByClassName("active")[0];
 	dl = dd.parentElement;
 	index = $(".active").index();
@@ -40,33 +38,36 @@ function getAlltime(h,m,s){
 	return 1000*(60*(h*60+m)+s)+5*1000;//加5秒延迟，因为启动脚本到点击播放按钮有时差，防止下次加载失败。
 }
 function init(){
-		data = datapart[i];
-		if(data!=undefined){
-			data=data.innerHTML;
-		}else{
-			i=0;
-			data = datapart[i].innerHTML;
-		}
-		if(!patt.test(data)){
-			console.clear();
-			console.info("找不到时间信息!");
-			console.info("PPT可以直接在下方输入最后一页页码，轻敲回车即可看完！");
-			console.info("方向键↑调出历史记录，轻敲回车可重启脚本。");
-			return;
-		}
-		    data = patt.exec(data)
-	    	data = data[0];
-		var args = data.split(":");
-		var s	 = args[args.length-1];
-		var m	 = args[args.length-2];
-		var h	 = args[args.length-3];
-		if(h==undefined){h=0;}
-		var time = getAlltime(h,m,s);
+	var patt = new RegExp("[0-9][0-9]:[0-9][0-9]");
+	datapart = document.getElementsByClassName("bottom-part");
+	var data;
+	data = datapart[i];
+	if(data!=undefined){
+		data=data.innerHTML;
+	}else{
+		i=0;
+		data = datapart[i].innerHTML;
+	}
+	if(!patt.test(data)){
 		console.clear();
-		console.info("正在播放："+$("em")[i].innerHTML);
-		console.info("视频总长："+h+"时"+m+"分"+s + "秒");
-		console.info("请在5秒内点击播放按钮，避免下节课加载失败！");
-		window.setTimeout("next();",time);
-		console.info("已设置自动提醒");
+		console.info("找不到时间信息!");
+		console.info("PPT可以直接在下方输入最后一页页码，轻敲回车即可看完！");
+		console.info("方向键↑调出历史记录，轻敲回车可重启脚本。");
+		return;
+	}
+	    data = patt.exec(data)
+	   	data = data[0];
+	var args = data.split(":");
+	var s	 = args[args.length-1];
+	var m	 = args[args.length-2];
+	var h	 = args[args.length-3];
+	if(h==undefined){h=0;}
+	var time = getAlltime(h,m,s);
+	console.clear();
+	console.info("正在播放："+$("em")[i].innerHTML);
+	console.info("视频总长："+h+"时"+m+"分"+s + "秒");
+	console.info("请在5秒内点击播放按钮，避免下节课加载失败！");
+	window.setTimeout("next();",time);
+	console.info("已设置自动提醒");
 }
 init();
